0696645f9eed97793716303ec20a724799d8d1e0
Uranka <39204253+urankab@users.noreply.github.com>
2020-07-12 22:18:38 -0600

    Working on Invoice Lines writing

--- 06-python/src/comp230-excel/program.py
+++ 06-python/src/comp230-excel/program.py
@@ -8,42 +8,70 @@ def requestInvoice():
 
     title_style = Font(name='Arial Rounded MT Bold', sz=25, b=True, color='A872DD')
     header_style = Font(name='Arial Rounded MT Bold', sz=14, b=True, color='8E44AD')
-    main_style = Font(name='Arial Rounded MT Bold', sz=12, b=True, color='5A49A5')
-    
+    customer_style = Font(name='Arial Rounded MT Bold', sz=12, b=True, color='5A49A5')
+    item_style = Font(name='Arial Rounded MT Bold', sz=10, b=True, color='8E44AD')
+
     inv_no = int(input('Please enter an invoice number: '))
 
     try:
         if inv_no == functions.invoice_list[inv_no]['invoice_no']:
+            # Customer Information
             customer_id = functions.invoice_list[inv_no]['customer_id']
             customer_name = str(functions.customer_list[customer_id]['f_name'] + ' ' +
-                functions.customer_list[customer_id]['l_name'])
+                                functions.customer_list[customer_id]['l_name'])
             customer_phone = str(functions.customer_list[customer_id]['phone'])
             customer_email = str(functions.customer_list[customer_id]['email'])
             customer_residence = str(functions.customer_list[customer_id]['street'] + ', ' +
-                functions.customer_list[customer_id]['city'])
+                                    functions.customer_list[customer_id]['city'])
             customer_zip = str(functions.customer_list[customer_id]['zipcode'])
+
+            # Invoice Lines & Products
+            invoice_lines = functions.invoice_line_list
+            product_ids = []
+
+            for key in invoice_lines:
+                if inv_no == invoice_lines[key]['invoice_no']:
+                    # print(invoice_lines[key]['product_id'])
+                    product_ids.append(invoice_lines[key]['product_id'])
+            # print(product_ids)
+
+            # Products
+            if len(product_ids) > 1:
+                for index in range(len(product_ids)):
+                    ids = product_ids[index]
+                    product_name = functions.product_list[ids]['name']
+                    print(product_name)
+
 
             workbook = Workbook()
             sheet = workbook.active
-            
-            sheet['G1'] = f'INVOICE {inv_no}'
-            sheet['G1'].alignment = Alignment(horizontal='center')
-            sheet['G1'].font = title_style
-            sheet['H2'] = "Uranka's Outdoors"
-            sheet['H2'].alignment = Alignment(horizontal='right')
-            sheet['H2'].font = header_style
+
+            # Sheet styling and writing
+            sheet['E2'] = "Uranka's Outdoors - Invoice"
+            sheet['E2'].alignment = Alignment(horizontal='center')
+            sheet['E2'].font = title_style
 
             sheet.column_dimensions['A'].width = 20
+            sheet.column_dimensions['B'].width = 20
+            sheet.column_dimensions['C'].width = 20
+            sheet.column_dimensions['D'].width = 20
+            sheet.column_dimensions['E'].width = 20
+            sheet.column_dimensions['F'].width = 20
+
+            for i in range(1, 8):
+                sheet['A' + str(i)].font = header_style
+                sheet['A' + str(i)].alignment = Alignment(horizontal='center')
+
             sheet['A1'] = 'Invoice No.'
             sheet['A2'] = 'Date: '
             sheet['A3'] = 'Customer: '
             sheet['A4'] = 'Contact: '
             sheet['A6'] = 'Residence: '
             sheet['A7'] = 'Zipcode: '
-            
-            for i in range(1,8):
-                sheet['A' + str(i)].font = header_style
-                sheet['A' + str(i)].alignment = Alignment(horizontal='center')
+
+            for i in range(1, 8):
+                sheet['B' + str(i)].font = customer_style
+                sheet['B' + str(i)].alignment = Alignment(horizontal='left')
 
             sheet['B1'] = functions.invoice_list[inv_no]['invoice_no']
             sheet['B2'] = functions.invoice_list[inv_no]['invoice_date']
@@ -53,29 +81,30 @@ def requestInvoice():
             sheet['B6'] = customer_residence
             sheet['B7'] = customer_zip
 
-            for i in range(1, 8):
-                sheet['B' + str(i)].font = main_style
-                sheet['B' + str(i)].alignment = Alignment(horizontal='left')
+            for col in sheet.iter_cols(min_row=11,max_col=6, max_row=11):
+                for cell in col:
+                    cell.font = item_style
 
             sheet['A11'] = 'Item'
-            sheet['A12'] =  # item name
-            
+            # sheet['A12'] =  # item name
+
             sheet['B11'] = 'Description'
-            sheet['B12'] =  # desc
-            
+            # sheet['B12'] =  # desc
+
             sheet['C11'] = 'Quantity'
-            sheet['C12'] =  # quantity
-            
+            # sheet['C12'] =  # quantity
+
             sheet['D11'] = 'Unit Price'
-            sheet['D12'] =  # price
-            
+            # sheet['D12'] =  # price
+
             sheet['E11'] = 'Amount'
-            sheet['E12'] =  # amount
+            # sheet['E12'] =  # amount
 
             sheet['F11'] = 'Amount Due'
-            sheet['F12'] =  # total
-            
-            workbook.save(f'/code/cohort4/06-python/src/comp230-excel/invoice_{inv_no}.xlsx')
+            # sheet['F12'] =  # total
+
+            workbook.save(
+                f'/code/cohort4/06-python/src/comp230-excel/invoice_{inv_no}.xlsx')
             print(f'Invoice has been written to invoice_{inv_no}.xlsx')
     except:
         print('Could not create invoice')
