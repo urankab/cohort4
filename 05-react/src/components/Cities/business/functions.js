beae6b7a74512c773276aac7049eb16c655bc799
Uranka <39204253+urankab@users.noreply.github.com>
2020-08-16 19:36:32 -0600

    updated url

--- 05-react/src/components/Cities/business/functions.js
+++ 05-react/src/components/Cities/business/functions.js
@@ -1,6 +1,7 @@
 import data from '../data.js'
 
-const url = 'http://localhost:5000/';
+// const url = 'http://localhost:5000/';
+const url = 'https://urankab.github.io/cities/'
 // const url = 'https://my-react-api.herokuapp.com/'
 // const url = 'https://bf40bw8dz2.execute-api.ca-central-1.amazonaws.com/dev/hello-world'
 
