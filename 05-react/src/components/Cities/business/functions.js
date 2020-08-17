342d4ec4b474c9598c44e5eccefb37778fcb5c5b
Uranka <39204253+urankab@users.noreply.github.com>
2020-08-16 20:22:46 -0600

    changed url

--- 05-react/src/components/Cities/business/functions.js
+++ 05-react/src/components/Cities/business/functions.js
@@ -1,6 +1,7 @@
 import data from '../data.js'
 
-const url = 'http://localhost:5000/';
+// const url = 'http://localhost:5000/';
+const url = 'https://my-react-api.herokuapp.com/'
 
 async function postData(url = '', data = {}) {
     // Default options are marked with *
